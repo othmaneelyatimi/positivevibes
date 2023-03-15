36d29f0e3d8dee692c36072ac02ff433d7b7126a
Othmane Elyatimi <101941750+othmaneelyatimi@users.noreply.github.com>
2023-03-15 02:18:50 +0100

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,26 @@
+<?php
+include 'connexion.php';
+if (isset($_POST['name']) && isset($_POST['email']) && isset($_POST['subject']) && isset($_POST['message'])) {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $subject = $_POST['subject'];
+    $message = $_POST['message'];
+
+    // echo "<script type='text/javascript'>alert('$message');</script>";
+} else {
+    $message = "dead.";
+    echo "<script type='text/javascript'>alert('$message');</script>";
+    die();
+}
+$q = mysqli_query(mysqli_connect("localhost", "root", "", "POSITIVEVIBES"), "INSERT INTO contact VALUES ('','$name','$email','$subject','$message')");
+if ($q) {
+    $message = "Subject added.";
+    echo "<script type='text/javascript'>alert('$message');</script>";
+    header("Location:index.html");
+} else {
+    $message = "Username and/or Password incorrect.\\nTry again.";
+    echo "<script type='text/javascript'>alert('$message');</script>";
+    // header("Location:index.php");
+
+}
+?>
